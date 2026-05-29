345b66348ca266d532ccad7dbd9d2586b4eb293b
Jonathon Chew <jonchew626@hotmail.com>
2026-05-29 19:32:56 +0100

    refactor: logic looking at the positional argument pased in, converted to a dict which can be overwritten or reused by other logic

--- src/api_client_cli/main.py
+++ src/api_client_cli/main.py
@@ -1,11 +1,11 @@
-import sys
-import os
-import json
 import argparse
+import json
+import os
+import sys
 
 import requests
 
-FILE_NAME = "./requests.json"
+FILE_NAME = os.path.expanduser("~/.api_client/requests.json")
 
 
 def _file_exsits() -> bool:
@@ -15,27 +15,34 @@ def _file_exsits() -> bool:
         return False
 
 
-def arg_to_dict(input: list[str]) -> dict[str, str]:
+def arg_to_dict(input: str | list[str] | None) -> dict[str, str]:
+    if input is None:
+        return {}
+
     return_dict = {}
 
-    for i in input:
-        key, value = i.split(":", 1)
+    if isinstance(input, list):
+        for i in input:
+            key, value = i.split(":", 1)
+            return_dict[key] = value
+    elif isinstance(input, str):
+        key, value = input.split(":", 1)
         return_dict[key] = value
 
     return return_dict
 
 
-def save_argument(arg_name: str):
+def save_argument(arg_name: str, values: dict):
     if _file_exsits():
         with open(FILE_NAME, "r") as r:
             contents = json.loads(r.read())
 
         if contents.get(arg_name, None) == None:
-            contents[arg_name] = args
+            contents[arg_name] = values
         else:
             userChoice = input(f"Do you want to replace: {contents[arg_name]}")
             if userChoice.lower() == "y" or userChoice.lower() == "yes":
-                contents[arg_name] = args
+                contents[arg_name] = values
             else:
                 return
 
@@ -44,7 +51,7 @@ def save_argument(arg_name: str):
 
     else:
         with open(FILE_NAME, "w") as w:
-            w.write("")
+            w.write(json.dumps({arg_name: values}))
     pass
 
 
@@ -59,17 +66,23 @@ def get_argument(arg_name: str) -> dict[str, str]:
         print(f"No saved request for: {arg_name}")
     else:
         print(contents[arg_name])
-    
+
     return contents[arg_name]
 
-def run_argument(arg_name: str):
+
+def run_argument(arg_name: str) -> dict[str, str]:
     args = get_argument(arg_name)
+    return args
+
 
 def list_argument():
     if not _file_exsits():
         return
-    pass
+    
+    with open(FILE_NAME, "r") as r:
+        print(json.loads(r.read()))
 
+    return
 
 def delete_argument(arg_name: str):
     if not _file_exsits():
@@ -78,22 +91,158 @@ def delete_argument(arg_name: str):
 
 
 def main():
+
+    flags = argparse.ArgumentParser()
+
+    ## Managing save requests
+    subparsers = flags.add_subparsers(dest="command", required=True)
+
+    ## ===================
+    ## Send Request
+    ## ===================
+    send_parser = subparsers.add_parser("send")
+    # Manditories
+    send_parser.add_argument(
+        "--method",
+        choices=["GET", "POST", "DELETE", "PUT"],
+        required=True,
+        default="GET",
+        help="HTTP Method to make the request with",
+    )
+    send_parser.add_argument("--URI", required=True, help="Destination")
+
+    # Optinal
+    send_parser.add_argument("--headers", nargs="+", help="HTTP headers")
+    send_parser.add_argument("--body", help="HTTP Body as a string")
+    send_parser.add_argument("--json-body", help="JSON Body")
+    send_parser.add_argument("--params", help="HTTP parameters /?param=", nargs="+")
+    send_parser.add_argument("--output", help="denote a file to save the response to")
+
+    ## ===================
+    ## Save Request
+    ## ===================
+    save_parse = subparsers.add_parser("save")
+    # Manditories
+    save_parse.add_argument("name")  # postistional argument
+    save_parse.add_argument(
+        "--method",
+        choices=["GET", "POST", "DELETE", "PUT"],
+        required=True,
+        default="GET",
+        help="HTTP Method to make the request with",
+    )
+    save_parse.add_argument("--URI", required=True, help="Destination")
+
+    # Optinal
+    save_parse.add_argument("--headers", nargs="+", help="HTTP headers")
+    save_parse.add_argument("--body", help="HTTP Body as a string")
+    save_parse.add_argument("--json-body", help="JSON Body")
+    save_parse.add_argument("--params", help="HTTP parameters /?param=", nargs="+")
+    save_parse.add_argument("--output", help="denote a file to save the response to")
+
+    ## ===================
+    ## Get Request
+    ## ===================
+    get_parse = subparsers.add_parser("get")
+    get_parse.add_argument("name")
+
+    ## ===================
+    ## Run Request
+    ## ===================
+    get_parse = subparsers.add_parser("run")
+    get_parse.add_argument("name")
+
+    ## ===================
+    ## List Saved Requests
+    ## ===================
+    list_parse = subparsers.add_parser("list")
+
+    ## ===================
+    ## Delete Saved Request
+    ## ===================
+    delete_parse = subparsers.add_parser("delete")
+    delete_parse.add_argument("name")
+
+    # Utilities
+    flags.add_argument(
+        "--silent", action="store_true", help="Print key output to the screen"
+    )
+
+    args = flags.parse_args()
+
+    values = {}
+
+    if args.command == "send":
+        if args.json_body and args.body:
+            print("There are 2 different types of bodies, please choose only one")
+            sys.exit(1)
+
+        values = {
+            "method": getattr(args, "method", None),
+            "URI": getattr(args, "URI", None),
+            "headers": getattr(args, "headers", None),
+            "body": getattr(args, "body", None),
+            "json_body": getattr(args, "json_body", None),
+            "params": getattr(args, "params", None),
+            "output": getattr(args, "output", None),
+        }
+
+        values = {key: value for key, value in values.items() if value is not None}
+    elif args.command == "save":
+        if args.json_body and args.body:
+            print("There are 2 different types of bodies, please choose only one")
+            sys.exit(1)
+
+        values = {
+            "method": getattr(args, "method", None),
+            "URI": getattr(args, "URI", None),
+            "headers": getattr(args, "headers", None),
+            "body": getattr(args, "body", None),
+            "json_body": getattr(args, "json_body", None),
+            "params": getattr(args, "params", None),
+            "output": getattr(args, "output", None),
+        }
+
+        values = {key: value for key, value in values.items() if value is not None}
+        save_argument(args.name, values)
+        return
+    elif args.command == "get":
+        _ = get_argument(args.name)
+        return
+    elif args.command == "list":
+        list_argument()
+        return
+    elif args.command == "run":
+        values = run_argument(args.name)
+    elif args.command == "delete":
+        delete_argument(args.name)
+        return
+
     r = requests.Response
 
-    params = arg_to_dict(args.params) if args.params else None
-    headers = arg_to_dict(args.headers) if args.headers else None
     if args.json_body:
-        body = args.json_body
+        body = arg_to_dict(args.json_body)
     elif args.body:
         body = args.body
     else:
         body = None
 
-    destination = args.URI
+    methods = values.get("method", None)
+    destination = values.get("URI", None)
+    params = arg_to_dict(values.get("params", None))
+    headers = arg_to_dict(values.get("headers", None))
 
-    match args.method:
+    print(body)
+
+    assert destination
+    assert methods
+
+    if not isinstance(headers, dict) and headers is not None:
+        return
+
+    match methods:
         case "GET":
-            r = requests.get(args.URI, params=params, headers=headers)
+            r = requests.get(destination, params=params, headers=headers)
             r.raise_for_status()
 
         case "POST":
@@ -167,100 +316,4 @@ def main():
 
 
 if __name__ == "__main__":
-    flags = argparse.ArgumentParser()
-
-    ## Managing save requests
-    subparsers = flags.add_subparsers(dest="command", required=True)
-
-    ## ===================
-    ## Send Request
-    ## ===================
-    send_parser = subparsers.add_parser("send")
-    # Manditories
-    send_parser.add_argument(
-        "--method",
-        choices=["GET", "POST", "DELETE", "PUT"],
-        required=True,
-        default="GET",
-        help="HTTP Method to make the request with",
-    )
-    send_parser.add_argument("--URI", required=True, help="Destination")
-
-    # Optinal
-    send_parser.add_argument("--headers", nargs="+", help="HTTP headers")
-    send_parser.add_argument("--body", help="HTTP Body as a string")
-    send_parser.add_argument("--json-body", help="JSON Body")
-    send_parser.add_argument("--params", help="HTTP parameters /?param=", nargs="+")
-    send_parser.add_argument("--output", help="denote a file to save the response to")
-
-    ## ===================
-    ## Save Request
-    ## ===================
-    save_parse = subparsers.add_parser("save")
-    # Manditories
-    save_parse.add_argument(
-        "--method",
-        choices=["GET", "POST", "DELETE", "PUT"],
-        required=True,
-        default="GET",
-        help="HTTP Method to make the request with",
-    )
-    save_parse.add_argument("--URI", required=True, help="Destination")
-    save_parse.add_argument("--name", required=True)
-
-    # Optinal
-    save_parse.add_argument("--headers", nargs="+", help="HTTP headers")
-    save_parse.add_argument("--body", help="HTTP Body as a string")
-    save_parse.add_argument("--json-body", help="JSON Body")
-    save_parse.add_argument("--params", help="HTTP parameters /?param=", nargs="+")
-    save_parse.add_argument("--output", help="denote a file to save the response to")
-
-    ## ===================
-    ## Get Request
-    ## ===================
-    get_parse = subparsers.add_parser("get")
-    get_parse.add_argument("--name", required=True)
-
-    ## ===================
-    ## Run Request
-    ## ===================
-    get_parse = subparsers.add_parser("run")
-    get_parse.add_argument("--name", required=True)
-
-
-    ## ===================
-    ## List Saved Requests
-    ## ===================
-    list_parse = subparsers.add_parser("list")
-
-
-    ## ===================
-    ## Delete Saved Request
-    ## ===================
-    delete_parse = subparsers.add_parser("delete")
-    delete_parse.add_argument("--name", required=True)
-
-    # Utilities
-    flags.add_argument(
-        "--silent", action="store_true", help="Print key output to the screen"
-    )
-
-    args = flags.parse_args()
-
-    if args.json_body and args.body:
-        print("There are 2 different types of bodies, please choose only one")
-        sys.exit(1)
-
-    if args.command == "send":
-        main()
-    elif args.command == "save":
-        save_argument(args.name)
-        main()
-    elif args.command == "get":
-        _ = get_argument(args.name)
-    elif args.command == "list":
-        list_argument()
-    elif args.command == "run":
-        run_argument(args.name)
-    elif args.command == "delete":
-        delete_argument(args.name)
+    main()
